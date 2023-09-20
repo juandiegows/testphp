f3c2909b3bec47066eaf3709892948dcea0d1a59
JuanDiegoWS <mejiamaestrejuandiego@gmail.com>
2023-09-19 19:25:03 -0500

    sin errores

--- conexion.php
+++ conexion.php
@@ -10,5 +10,3 @@ if ($mysqli->connect_errno) {
     echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
 echo $mysqli->host_info . "\n";
-
-$mbd = new PDO('mysql:host=localhost;dbname=prueba', $usuario, $contraseña);
